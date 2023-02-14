ebd13c46aff40a6dd633b3e5abb8df33cd8542ac
siverNa <dhrru123@naver.com>
2023-02-14 17:59:07 +0900

    zf1 - Auth 클래스 작업
    작업한 내용 :
    Was_Auth_Storage 코드 추가

--- library/Was/Auth/Storage.php
+++ library/Was/Auth/Storage.php
@@ -69,16 +69,83 @@ class Was_Auth_Storage implements Zend_Auth_Storage_Interface {
      * {@inheritDoc}
      * @see Zend_Auth_Storage_Interface::read()
      */
-    public function read()
-    {}
+    public function read() {
+        //매개변수로 넣은 기본키의 row 객체 반환
+        return $this->_accessTable->find($this->_accessPk)->current();
+    }
 
-    public function isEmpty()
-    {}
+    /**
+     * 
+     * {@inheritDoc}
+     * @see Zend_Auth_Storage_Interface::isEmpty()
+     */
+    public function isEmpty() {
+        //매개변수로 기본키를 넣어, 갯수 반환
+        return $this->_accessTable->find($this->_accessPk)->count() > 0 ? false : true;
+    }
 
-    public function clear()
-    {}
-
-    public function write($contents)
-    {}
+    /**
+     * 검색한 row객체 delete, pk 초기화
+     * {@inheritDoc}
+     * @see Zend_Auth_Storage_Interface::clear()
+     */
+    public function clear() {
+        $access = $this->read();
+        
+        if ($access) {
+            $access->delete();
+            $this->_accessPk = null;
+        }
+    }
+    
+    /**
+     * 
+     * {@inheritDoc}
+     * @see Zend_Auth_Storage_Interface::write()
+     * 
+     * @param mix identity 정보
+     */
+    public function write($contents) {
+        if (!array_key_exists('identityId', $contents) || is_null($contents['identityId'] || empty($contents['identityId']))) {
+            /**
+             * @see Was_Auth_Exception
+             */
+            require_once 'Was/Auth/Exception.php';
+            throw new Was_Auth_Exception("identityId 가 비어있습니다.");
+        }
+        //만약 입력된 remoteIp가 있으면 그걸로 설정, 없으면 $_SERVER['REMOTE_ADDR'] 로 설정
+        $remoteAddr = (isset($contents['remoteIp']) && !empty($contents['remoteIp'])) ? $contents['remoteIp'] : $_SERVER['REMOTE_ADDR'];
+        
+        //만약 입력된 session 정보가 있으면 그걸로 설정, 없으면 기본 sessionId로 설정
+        $this->_session = (isset($contents['sessionId']) && !empty($contents['sessionId'])) ? $contents['sessionId'] : $this->_session;
+        
+        $contents['remoteIp'] = $remoteAddr;
+        $contents['sessionId'] = $this->_session;
+        $contents['authTime'] = new Zend_Db_Expr('NOW()');
+        $contents['accessTime'] = new Zend_Db_Expr('NOW()');
+        
+        //db_table 의 insert 메소드 수행 후, 해당 레코드의 pk를 리턴받음
+        $this->_accessPk = $this->_accessTable->insert($contents);
+        
+        if ($this->_accessPk) {
+            $this->_historyTable->insert(array(
+                'identityId'    => $contents['identityId'],
+                'remoteIp'      => $contents['remoteIp'],
+                'authTime'      => new Zend_Db_Expr('NOW()'),
+                'result'        => self::LOGIN_SUCCESS
+            ));
+        } else {
+            $this->_accessPk = 0;
+            
+            $this->_historyTable->insert(array(
+                'identityId'    => $contents['identityId'],
+                'remoteIp'      => $contents['remoteIp'],
+                'authTime'      => new Zend_Db_Expr('NOW()'),
+                'result'        => self::LOGIN_FAIL
+            ));
+        }
+        
+        return $this->_accessPk;
+    }
 }
 
