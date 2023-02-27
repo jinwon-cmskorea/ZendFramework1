c657a7707d147c26f4b743e2631698a6633c7959
siverNa <dhrru123@naver.com>
2023-02-27 17:10:45 +0900

    zf1 - 아이디 찾기 form 작업
    작업한 내용 :
    아이디 찾기 form 클래스 제작,
    form 클래스 테스트 코드 작성

---
+++ library/Was/Member/Form/SearchId.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @see Zend_Form
+ */
+require_once 'Zend/Form.php';
+/**
+ * @see Zend_Validate_Regex
+ */
+require_once 'Zend/Validate/Regex.php';
+/**
+ * Was_Member_Form_SearchId
+ *
+ * @package    Was
+ * @subpackage Was_Member_Form
+ */
+class Was_Member_Form_SearchId extends Zend_Form {
+    /*
+     * {@inheritDoc}
+     * @see Zend_Form::init()
+     */
+    public function init() {
+        $decorators = array(
+            "Description",
+            "FormElements",
+            array("Form", array(
+                'class'     => 'form-horizontal loginForm',
+                'name'      => 'search-id-form'
+            ))
+        );
+        //form 데코레이터 설정
+        $this->setDecorators($decorators);
+        //element 데코레이터 설정
+        $this->setElementDecorators(array(
+            "ViewHelper",
+            "Description",
+            array("Label", array('class' => 'col-sm-2 control-label-left')),
+            array("HtmlTag", array("tag" => "div", 'class' => 'form-group'))
+        ));
+        
+        $this->addElement('text', 'name', array(
+            'class'     => 'myForm-control',
+            'required'  => true,
+            'label'     => '이름'
+        ));
+        $name = $this->getElement('name');
+        $name->addValidator(new Zend_Validate_Regex('/[가-힣A-Za-z]+$/'));
+        
+        $this->addElement('text', 'telNumber', array(
+            'class'     => 'myForm-control',
+            'required'  => true,
+            'label'     => '휴대전화번호'
+        ));
+        $phone = $this->getElement('telNumber');
+        $phone->addValidator(new Zend_Validate_Regex('/^01(0|1|6|7|8|9)-?([0-9]{3,4})-?([0-9]{4})$/'));
+        
+        $this->addElement('submit', 'search', array(
+            'class' => 'btn-block login-btn',
+            'label' => '아이디 찾기'
+        ));
+        $login = $this->getElement('search');
+        $login->removeDecorator('HtmlTag');
+        $login->removeDecorator('Label');
+        
+        
+    }
+}
+
---
+++ tests/library/Was/Member/Form/SearchIdTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Was Member Form SearchId test file
+ */
+/*
+ * require bootstrap
+ */
+require_once __DIR__.'/bootstrap.php';
+/*
+ * require test class
+ */
+require_once 'Was/Member/Form/SearchId.php';
+/**
+ * Was_Member_Form_SearchId test case.
+ */
+class Was_Member_Form_SearchIdTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     *
+     * @var Was_Member_Form_SearchId
+     */
+    private $form;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp() {
+        parent::setUp();
+
+        // TODO Auto-generated Was_Member_Form_SearchIdTest::setUp()
+
+        $this->form = new Was_Member_Form_SearchId(/* parameters */);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown() {
+        // TODO Auto-generated Was_Member_Form_SearchIdTest::tearDown()
+        $this->form = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct() {
+        // TODO Auto-generated constructor
+    }
+
+    /**
+     * Tests Was_Member_Form_SearchId->init()
+     */
+    public function testInit() {
+        $element = $this->form->getElement('name');
+        $this->assertInstanceOf('Zend_Form_Element_text', $element);
+        $this->assertEquals('name', $element->getId());
+        $this->assertEquals('name', $element->getName());
+        $this->assertEquals(true, $element->isRequired());
+        $this->assertEquals('이름', $element->getLabel());
+        $this->assertEquals('myForm-control', $element->getAttrib('class'));
+        $this->assertInstanceOf('Zend_Validate_Regex', $element->getValidator('regex'));
+        
+        $element = $this->form->getElement('telNumber');
+        $this->assertInstanceOf('Zend_Form_Element_text', $element);
+        $this->assertEquals('telNumber', $element->getId());
+        $this->assertEquals('telNumber', $element->getName());
+        $this->assertEquals(true, $element->isRequired());
+        $this->assertEquals('휴대전화번호', $element->getLabel());
+        $this->assertEquals('myForm-control', $element->getAttrib('class'));
+        $this->assertInstanceOf('Zend_Validate_Regex', $element->getValidator('regex'));
+        
+        $element = $this->form->getElement('search');
+        $this->assertInstanceOf('Zend_Form_Element_Submit', $element);
+        $this->assertEquals('search', $element->getId());
+        $this->assertEquals('search', $element->getName());
+        $this->assertEquals('btn-block login-btn', $element->getAttrib('class'));
+    }
+}
+
