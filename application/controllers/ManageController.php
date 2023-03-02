dfe099a1f831cb3e63d46494fcd0c8af03481e26
siverNa <dhrru123@naver.com>
2023-03-02 15:11:59 +0900

    zf1 - 회원관리 페이지 작업
    작업한 내용 :
    join 및 pagination 객체 생성 코드를 if문 안으로 이동,
    controls.phtml 코드 수정,
    버튼 디자인

--- application/controllers/ManageController.php
+++ application/controllers/ManageController.php
@@ -24,27 +24,28 @@ class ManageController extends Zend_Controller_Action {
     public function manageAction() {
         $request = $this->getRequest();
         
-        $identityTable = new Was_Auth_Table_Identity();
-        $memberTable = new Was_Member_Table_Member();
-        //table join 작업
-        $select = $memberTable->select();
-        $select->from(array('a' => $memberTable->getTableName()), array('pk', 'id', 'name', 'telNumber', 'email', 'position'))
-               ->join(array('b' => $identityTable->getTableName()), "a.id = b.id")
-               ->order("a.pk DESC");
-        //Zend_Db_Table_Select 객체로 join을 사용할 때, 아래와 같이 설정해줘야함
-        $select->setIntegrityCheck(false);
-        //join 결과를 array로 가져옴
-        $result = $memberTable->getAdapter()->fetchAll($select);
-        
-        //paginator 객체 생성
-        $paginator = Zend_Paginator::factory($result);
-        //현재 페이지를 _getParam 을 이용해 설정해줌(2번째 인수는 default로 설정할 값)
-        $paginator->setCurrentPageNumber($this->_getParam('page', 1));
-        
-        $this->view->paginator = $paginator;
-        $this->view->totalCount = count($result);
-        $this->view->recordCount = count($result);
-        
+        if ($this->getRequest()) {
+            $identityTable = new Was_Auth_Table_Identity();
+            $memberTable = new Was_Member_Table_Member();
+            //table join 작업
+            $select = $memberTable->select();
+            $select->from(array('a' => $memberTable->getTableName()), array('pk', 'id', 'name', 'telNumber', 'email', 'position'))
+            ->join(array('b' => $identityTable->getTableName()), "a.id = b.id")
+            ->order("a.pk DESC");
+            //Zend_Db_Table_Select 객체로 join을 사용할 때, 아래와 같이 설정해줘야함
+            $select->setIntegrityCheck(false);
+            //join 결과를 array로 가져옴
+            $result = $memberTable->getAdapter()->fetchAll($select);
+            
+            //paginator 객체 생성
+            $paginator = Zend_Paginator::factory($result);
+            //현재 페이지를 _getParam 을 이용해 설정해줌(2번째 인수는 default로 설정할 값)
+            $paginator->setCurrentPageNumber($this->_getParam('page', 1));
+            
+            $this->view->paginator = $paginator;
+            $this->view->totalCount = $paginator->getTotalItemCount();
+            $this->view->recordCount = count($result);
+        }
         
         $form = new Was_Member_Form_Manage();
         //select 요소의 option 설정
