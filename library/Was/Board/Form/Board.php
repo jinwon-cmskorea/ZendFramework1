70058486581cc8bca69bb348816aca4542dc85f3
siverNa <69504543+siverNa@users.noreply.github.com>
2023-03-19 22:37:39 +0900

    zf1 - 게시글 작성 작업
    작업한 내용 :
    게시글 작성 페이지 디자인,
    board form 코드 작업,
    css 수정

--- library/Was/Board/Form/Board.php
+++ library/Was/Board/Form/Board.php
@@ -20,7 +20,7 @@ class Was_Board_Form_Board extends Zend_Form {
      */
     public function init() {
         $decorators = array(
-            "Description", "FormElements", "Fieldset", "Form"
+            "Description", "FormElements", "Form"
         );
         $this->setDecorators($decorators);
         
@@ -29,34 +29,59 @@ class Was_Board_Form_Board extends Zend_Form {
             "ViewHelper",
             "Errors",
             "Description",
-            "Label",
-            array("HtmlTag", array("tag" => "div"))
+            array("Label", array("class" => "control-label-center")),
+            array("HtmlTag", array("tag" => "div", "class" => "form-group"))
         ));
         
         $this->addElement('text', 'title', array(
             'required'  => true,
-            'label'     => '제목'
+            'class'     => 'write-board-title space-form',
+            'label'     => '제 목'
         ));
         
         $this->addElement('textarea', 'content', array(
             'required'  => true,
-            'label'     => '내용'
+            'class'     => 'write-board-textarea space-form',
+            'label'     => '내 용'
         ));
         
         $this->addElement('text', 'writer', array(
             'required'  => true,
+            'class'     => 'write-board-input space-form input-writer',
             'label'     => '작성자'
         ));
         $writer = $this->getElement('writer');
         $writer->addValidator(new Zend_Validate_Regex('/[가-힣A-Za-z]+$/'));
         
         $this->addElement('file', 'uploadFile', array(
-            'label'     => '파일업로드'
+            'label'     => '파일업로드',
+            'class'     => 'space-form'
         ));
         
-        $this->addElement('submit', 'submit', array('label' => '제출'));
+        //제출 및 취소 버튼의 label 과 HtmlTag 데코레이터 제거
+        $this->addElement('submit', 'submit', array(
+            'class'     => 'submit-btn'
+        ));
+        $submit = $this->getElement('submit');
+        $submit->removeDecorator('label');
+        $submit->removeDecorator('HtmlTag');
         
-        $this->addElement('button', 'cancle', array('label' => '취소'));
+        $this->addElement('button', 'cancle', array(
+            'label'     => '취 소',
+            'class'     => 'cancle-btn'
+        ));
+        $cancle = $this->getElement('cancle');
+        $cancle->removeDecorator('label');
+        $cancle->removeDecorator('HtmlTag');
+        
+        //작성, 취소 버튼을 묶어주기 위해 DisplayGroup 사용
+        $this->addDisplayGroup(array('submit', 'cancle'), 'btns');
+        $group = $this->getDisplayGroup('btns');
+        $group->clearDecorators();
+        $group->addDecorators(array(
+            'FormElements',
+            array('HtmlTag', array('tag' => 'div', 'class' => 'write-button'))
+        ));
     }
 }
 
